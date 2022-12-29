1e2050314d53b9677ef81028fe128a1ea9e8e67c
Nix <73953379+NixWasHere@users.noreply.github.com>
2022-12-29 15:23:29 +0100

    Update client.py

--- client.py
+++ client.py
@@ -20,22 +20,30 @@ def connect_to_server(host, port):
 def main():
     host = str(input("IP: "))
     port = int(input("Port: "))
+    
     while True:
         try:
+            
             conn = connect_to_server(host, port)
+            
             while True:
+                
                 command = input("Enter command: ")
+                
                 if (command == "exit"):
                     print("Leaving...")
                     exit()
+                    
                 try:
                     send_command(conn, command)
                     receive_output(conn)
+                    
                 except ConnectionResetError:
                     print("Connection Reset, retrying...")
                     time.sleep(5)
                     conn = connect_to_server(host, port)
                     continue
+                    
         except ConnectionRefusedError:
             print("Server offline, retrying in 5 seconds..")
             time.sleep(5)
